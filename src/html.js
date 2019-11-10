f34c6abf2df1aaa8ab04cde41892c37968918141
Angel Mendez <angelmendezz751@gmail.com>
2019-11-10 01:51:26 -0600

    ADD PLUGINS

--- src/html.js
+++ src/html.js
@@ -1,10 +1,10 @@
 import React from "react"
 import PropTypes from "prop-types"
-
 export default function HTML(props) {
   return (
     <html {...props.htmlAttributes}>
       <head>
+        <tag />
         <meta charSet="utf-8" />
         <meta httpEquiv="x-ua-compatible" content="ie=edge" />
         <meta
