923100c4bc8833de14ab6096e57c829ab6a3ee84
Jorge Castillo <jorgecastillo@waydda.com>
2019-11-10 12:55:26 -0600

    CSS issue solved

--- src/components/Header/Sidebar/index.js
+++ src/components/Header/Sidebar/index.js
@@ -8,17 +8,17 @@ const HeaderSidebar = ({isOpen, moveToContact}) => (
       <div style={{height: '88%'}} className='row u__no_margin justify-content-center align-items-center'>
         <div className='col-auto u__no_padding'>
           <Link to="/">
-            <h5 className="black_color_text">SERVICIOS</h5>
+            <h5 className="black_color_text sidebar_item">SERVICIOS</h5>
           </Link>
-          <h5 onClick={moveToContact} style={{cursor: 'pointer'}} className="black_color_text">CONTACTO</h5>
+          <h5 onClick={moveToContact} style={{cursor: 'pointer'}} className="black_color_text sidebar_item">CONTACTO</h5>
           <Link to="/free">
-            <h5 className="black_color_text u__no_margin">WEB GRATIS</h5>
+            <h5 className="black_color_text u__no_margin sidebar_item">WEB GRATIS</h5>
           </Link>
         </div>
       </div>
     </div>
     <style>{`
-      h5 {
+      .sidebar_item {
         font-size: 1.8em;
         margin: 0 0 1.5em 0 !important;
         text-align: center;
