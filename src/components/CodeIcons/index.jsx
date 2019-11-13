b4dd45bf0d1995370f01fc4d875653e608d5e766
Jorge Castillo <jorgecastillo@waydda.com>
2019-11-13 11:36:21 -0600

    CodeIcons

--- src/components/CodeIcons/index.jsx
+++ src/components/CodeIcons/index.jsx
@@ -6,59 +6,74 @@ import reactSVG from "../../images/react.svg"
 import rails from "../../images/rails-1.svg"
 import android from "../../images/android.svg"
 import ios from "../../images/apple.svg"
+import photoshop from '../../images/photoshop.svg';
+import premiere from '../../images/premiere.svg';
+import figma from '../../images/figma.svg';
 
-const CodeIcons = ({ big = false }) => (
-  <div className="col-xl-12 col-sm-11 u__no_padding">
-    <div className="row u__no_margin align-items-center">
+const CodeIcons = ({ big }) => (
+  <div className={`col-xl-12 ${big ? '' : 'col-sm-11'} u__no_padding`}>
+    <div className={`row u__no_margin align-items-center ${big ? 'justify-content-center' : ''}`}>
       <div
         data-tip="React js"
         data-scroll-hide={true}
         data-iscapture={false}
-        className={`col-xl-auto col-auto ${styles.main_image}`}
+        className={`col-xl-auto col-auto ${big ? '' : 'hide_on_mobile'} first_code_icon ${styles.main_image}`}
       >
-        <img src={reactSVG} />
+        <img src={reactSVG} className={big ? `${styles.code_icon_big} ${styles.icon_separate_from_bottom}` : styles.code_icon_small} />
       </div>
-      <div data-tip="Firebase" className={`col-xl-auto col-auto ${styles.main_image}`}>
-        <img src={firebaseSVG} />
+      <div data-tip="Firebase" className={`${big ? '' : 'hide_on_mobile'} col-xl-auto col-auto ${styles.main_image}`}>
+        <img src={firebaseSVG} className={big ? `${styles.code_icon_big} ${styles.icon_separate_from_bottom}` : styles.code_icon_small} />
       </div>
       <div
         data-tip="Google Cloud"
-        className={`col-xl-auto col-auto ${styles.main_image}`}
+        className={`col-xl-auto col-auto ${big ? '' : 'hide_on_small_mobile'} ${styles.main_image}`}
       >
-        <img src={cloud} />
+        <img src={cloud} className={big ? `${styles.code_icon_big} ${styles.icon_separate_from_bottom}` : styles.code_icon_small} />
       </div>
       <div
         data-tip="Ruby on Rails"
-        className={`col-xl-auto col-auto ${styles.main_image}`}
+        className={`col-xl-auto col-auto ${big ? '' : 'hide_on_mobile'} ${styles.main_image}`}
       >
-        <img src={rails} />
+        <img src={rails} className={big ? `${styles.code_icon_big} ${styles.icon_separate_from_bottom}` : styles.code_icon_small} />
       </div>
       <div
         data-tip="React Native Android Apps"
         className={`col-xl-auto col-auto ${styles.main_image}`}
       >
-        <img src={android} />
+        <img src={android} className={big ? `${styles.code_icon_big} ${styles.icon_separate_from_bottom}` : styles.code_icon_small} />
       </div>
       <div
         data-tip="React Native IOS Apps"
         className={`col-xl-auto col-auto ${styles.main_image}`}
       >
-        <img
-          src={ios}
-          style={{
-            height: "30px",
-          }}
-        />
+        <img src={ios} className={big ? `${styles.code_icon_big} ${styles.icon_separate_from_bottom}` : styles.code_icon_small} />
+      </div>
+      <div
+        data-tip="Figma"
+        className={`col-xl-auto col-auto ${styles.main_image}`}
+      >
+        <img src={figma} className={big ? `${styles.code_icon_big} ${styles.icon_separate_from_bottom}` : styles.code_icon_small} />
+      </div>
+      <div
+        data-tip="Adobe Photoshop"
+        className={`col-xl-auto col-auto ${styles.main_image}`}
+      >
+        <img src={photoshop} className={big ? `${styles.code_icon_big} ${styles.icon_separate_from_bottom}` : styles.code_icon_small} />
+      </div>
+      <div
+        data-tip="Adobe Premiere"
+        className={`col-xl-auto col-auto ${styles.main_image}`}
+      >
+        <img src={premiere} className={big ? `${styles.code_icon_big} ${styles.icon_separate_from_bottom}` : styles.code_icon_small} />
       </div>
     </div>
     <style>{`
-        @media(min-width: 576px) and (max-width: 768px) {
-          .react-icon {
-            padding-left: 0;
-           
-          }
+      @media screen and (min-width: 767px) {
+        .first_code_icon {
+          padding-left: 0;
         }
-      `}</style>
+      }
+    `}</style>
   </div>
 )
 
