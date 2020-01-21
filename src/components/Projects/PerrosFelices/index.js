58d470ea49db2aa74fa4e894365ee93dab645329
Jorge Castillo <jorgecastillo@waydda.com>
2020-01-20 22:47:05 -0600

    Perros Felices

--- src/components/Projects/PerrosFelices/index.js
+++ src/components/Projects/PerrosFelices/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styles from './styles.module.css';
+import { FaGooglePlay } from 'react-icons/fa';
 import perrosFelicesImage from '../../../images/PerrosFelices.png';
 
 const ProjectsPerrosFelices = ({}) => (
@@ -12,9 +13,13 @@ const ProjectsPerrosFelices = ({}) => (
             <p className={styles.perros_felices_phrase}>Adopta, ayuda y gana un amigo</p>
             <div className="separator" style={{backgroundColor: 'rgb(94, 46, 171)', margin: '1em 0'}} />
             <p className={styles.perros_felices_description}>Perros Felices facilita la tarea de conectar personas y albergues que tienen en adopción perros con personas que quieren un nuevo compañero.</p>
-            <button className={`u__main_box_shadow ${styles.dicover_how_button}`}>
-              DESCUBRE CÓMO
-            </button>
+            <a
+              className={`u__main_box_shadow ${styles.dicover_how_button}`}
+              href="https://play.google.com/store/apps/details?id=com.perrosfelices"
+              target="_blank"
+            >
+              <FaGooglePlay size={20} color="currentColor" style={{ marginRight: '0.5em' }} /> DISPONIBLE EN GOOGLE PLAY
+            </a>
           </div>
         </div>
         <div className={`col-12 col-sm-5 u__no_padding ${styles.perros_felices_image_container}`}>
