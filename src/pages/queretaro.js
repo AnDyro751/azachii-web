23de261854b23377b5780cf954b98ecae76102bf
Jorge Catillo <jjoorrssxp@outlook.com>
2020-02-18 02:28:42 -0600

    Init oaxaca

---
+++ src/pages/queretaro.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Layout from "../components/layout"
+
+function Queretaro() {
+  return (
+    <Layout footer_color="blue_light_color" footer_text="white_color_text">
+
+    </Layout>
+  );
+}
+
+export default Queretaro;
